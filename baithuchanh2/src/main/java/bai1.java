8b291441ba2a09d54c5db2640afde3f1e0fafa6c
tunglam024 <97438966+tunglam024@users.noreply.github.com>
2022-01-17 20:49:50 +0700

    Add files via upload

---
+++ baithuchanh2/src/main/java/bai1.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class bai1 {
+    public static void main(String[] args) throws Exception {
+        //doi tuong nhap suat du lieu 
+        Scanner sc = new Scanner(System.in);
+        //khai bao 1 bien de chua gia tri 
+        String strHT = "";
+        //nhap ten du lieu 
+        System.out.println("Nhap ho ten: ");
+        //gia tri nhap suat 
+        strHT = sc.nextLine();
+        System.out.println("Xin chao " + strHT);
+    }
+}
+        
